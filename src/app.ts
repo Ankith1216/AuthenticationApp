32c8f8d1dc8215c2ff998f46b56948db6381e04d
Ankith1216 <ankithchauhan01@gmail.com>
2023-01-04 16:02:04 +0530

    Minor Changes

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import api from './api';
-import pool from './config/db';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 api(app);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-});
-
-process.on('SIGINT', async () => {
-    console.log('Closing connection pool');
-    await pool.end();
-    process.exit(0);
 });
